38872fa588cb4f9b3c4bcf19324a5880038bbf1a
root <root@localhost.localdomain>
2018-08-08 10:57:44 +0800

    add test_shell custom command

--- src/mydjango/web/views.py
+++ src/mydjango/web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from utils.common import time_counter
+from utils.common import time_counter,get
 from utils.pmk import LinuxShell
 
 
@@ -14,7 +14,10 @@ def test_image(request):
     return render(request, 'web/test_image.html', locals())
 
 def test_shell(request):
+    command = get(request,'command')
+    if command == '':
+        command = 'date'
     kshell = LinuxShell('127.0.0.1')
     kshell.conn()
-    result = kshell.run_cmd('df -h')
+    result = kshell.run_cmd(command)
     return render(request, 'web/test_shell.html', locals())
